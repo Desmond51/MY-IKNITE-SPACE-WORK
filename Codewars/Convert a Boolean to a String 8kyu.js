19212821869e0273e65cc24aba5f15ea7f99729c
Desmond <desmondtekum9@gmail.com>
2022-05-24 10:29:14 +0100

    My third commit

---
+++ Codewars/Convert a Boolean to a String 8kyu.js
@@ -0,0 +1,11 @@
+
+ //  Convert a Boolean to a String (8kyu)
+/* Problems
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+
+//  Test
+digitize(35231),//[1,3,2,5,3])
+digitize(0),//[0])
---
+++ Codewars/Remove string spaces.js
@@ -0,0 +1,33 @@
+//   Remove String Spaces (8kyu)
+
+
+/*     Problem
+
+Simple, remove the spaces from the string, then return the resultant string.
+
+*/
+//    Steps
+/*
+- Create a function to call an our elements.
+- Give it a parameter cname x
+- Assign it to a new string 
+-remove the space from the string using Element.remove()
+
+
+*/
+
+function noSpace(x){
+    let newStr = x;
+    let final = newStr.remove("");
+    console.log(newStr.remove(""));
+    return final;
+   }
+   
+   removeStringSpace("");
+
+              // Sample Test
+
+              noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')//'8j8mBliB8gimjB8B8jlB');
+              noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd') //'88Bifk8hB8BB8BBBB888chl8BhBfd'); 
+              noSpace('8aaaaa dddd r     ') //'8aaaaaddddr'); 
+        
