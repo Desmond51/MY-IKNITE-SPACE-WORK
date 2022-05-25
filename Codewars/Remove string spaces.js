99aa8d4aa50b7841e3fb64407b3efc062113bed7
Desmond <desmondtekum9@gmail.com>
2022-05-26 00:51:36 +0100

    commit

--- Codewars/Remove string spaces.js
+++ Codewars/Remove string spaces.js
@@ -8,26 +8,27 @@ Simple, remove the spaces from the string, then return the resultant string.
 */
 //    Steps
 /*
-- Create a function to call an our elements.
-- Give it a parameter cname x
-- Assign it to a new string 
--remove the space from the string using Element.remove()
+- Create a function to call our noSpace.
+- Give it a parameter name x
+- Assign it to a  string 
+- use regular expression and replace function to remove spaces
+-Use the replace function with t
 
 
 */
 
 function noSpace(x){
     let newStr = x;
-    let final = newStr.remove("");
-    console.log(newStr.remove(""));
+    let final = newStr.replace(/\s+/g, ""); 
+    console.log(final);
     return final;
    }
    
-   removeStringSpace("");
+   noSpace("Say coding is good");
 
               // Sample Test
 
-              noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')//'8j8mBliB8gimjB8B8jlB');
-              noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd') //'88Bifk8hB8BB8BBBB888chl8BhBfd'); 
-              noSpace('8aaaaa dddd r     ') //'8aaaaaddddr'); 
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')//'8j8mBliB8gimjB8B8jlB');
+noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd') //'88Bifk8hB8BB8BBBB888chl8BhBfd'); 
+noSpace('8aaaaa dddd r     ') //'8aaaaaddddr'); 
         
