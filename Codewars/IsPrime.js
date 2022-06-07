de632036fab5db49d1bc82db860d45fae588c8c3
Desmond <desmondtekum9@gmail.com>
2022-06-07 05:49:30 +0100

    Codewars challenges

--- Codewars/IsPrime.js
+++ Codewars/IsPrime.js
@@ -21,10 +21,14 @@ is_prime(-1) // false //
 
 function isPrime(num) {
   //TODO
-if(num==2 ||)
+if((num==2 || num==3 ||num==5) || (num %2 !=0 && num %3 !=0 && num %5 !=0) ){
+return "true";
 
+} 
+else{
+	return "false";
 }
-
+}
 
 //  Test
 
--- Codewars/Sum of two lowest positive integers.js
+++ Codewars/Sum of two lowest positive integers.js
@@ -10,12 +10,26 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 
 
 function sumTwoSmallestNumbers(numbers) {  
-    numbers= numbers.sort();
-    for(let i =0; i<=numbers; i++ ) {
-
-        if(i>0){
-            return
-        }
+    //Code here
+    
+    let ordered = numbers.sort(function(a,b){return a-b;});
+  let result = 0;
+  
+  for (let i = 0; i < ordered.length; i++){
+    if (i===0){
+      result+=ordered[0];
     }
-
+    if (i===1){
+      result+=ordered[1];
+    }
   }
+  return result;
+  }
+
+
+  //  Test
+sumTwoSmallestNumbers([5, 8, 12, 19, 22])//, 13 , "Sum should be 13");
+sumTwoSmallestNumbers([15, 28, 4, 2, 43])//, 6 , "Sum should be 6");
+sumTwoSmallestNumbers([3, 87, 45, 12, 7])//, 10 , "Sum should be 10");
+sumTwoSmallestNumbers([23, 71, 33, 82, 1])//, 24 , "Sum should be 24");
+sumTwoSmallestNumbers([52, 76, 14, 12, 4])//, 16 , "Sum should be 16");
