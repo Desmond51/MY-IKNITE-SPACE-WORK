4f08ce00bd71a3d2350f339c1bb0c73887060215
Desmond <desmondtekum9@gmail.com>
2022-06-07 15:12:36 +0100

    FCC practice

---
+++ MyFreecodecampWork/BasicJavascriptAndAlgorithms/Basic Javascript/Generate Random Fractions with JavaScript.js
@@ -0,0 +1,16 @@
+/*   Question
+  Change randomFraction to return a random number instead of returning 0.
+*/
+
+function randomFraction() {
+    // Only change code below this line
+    let result = 0;
+    while(result == 0){
+      result = Math.random();
+      return result;
+    }
+  
+    return 4;
+  
+    // Only change code above this line
+  }
