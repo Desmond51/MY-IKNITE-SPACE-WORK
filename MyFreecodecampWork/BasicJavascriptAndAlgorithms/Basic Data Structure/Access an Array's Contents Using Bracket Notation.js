1bf9a39d9a6dbf0ccb1ee4720ca8135910336663
Desmond <desmondtekum9@gmail.com>
2022-06-16 19:19:06 +0100

    FCC commit

---
+++ MyFreecodecampWork/BasicJavascriptAndAlgorithms/Basic Data Structure/Access an Array's Contents Using Bracket Notation.js
@@ -0,0 +1,12 @@
+/*  Question
+
+In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+
+
+*/
+
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "Desmond";
+// Only change code above this line
+console.log(myArray);
