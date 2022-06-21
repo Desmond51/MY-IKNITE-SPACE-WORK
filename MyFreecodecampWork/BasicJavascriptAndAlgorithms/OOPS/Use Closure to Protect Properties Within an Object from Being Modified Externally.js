588302f8204909ab45136908dc2a8b559c2a9a83
Desmond <desmondtekum9@gmail.com>
2022-06-21 12:35:37 +0100

    Another commit on FCC

---
+++ MyFreecodecampWork/BasicJavascriptAndAlgorithms/OOPS/Use Closure to Protect Properties Within an Object from Being Modified Externally.js
@@ -0,0 +1,15 @@
+/*   Question
+
+Change how weight is declared in the Bird function so it is a private variable. Then, create a method getWeight that returns the value of weight 15.
+
+ */
+
+function Bird() {
+    let weight = 15;
+    this.getWeight = function(){
+      return weight;
+    };
+  
+  }
+  let palm = new Bird();
+  palm.getWeight();
