5abbce667361f7eb5ce3e18b0238f82b0ba2837b
Desmond <desmondtekum9@gmail.com>
2022-06-28 14:45:32 +0100

    Another commit

---
+++ MyFreecodecampWork/BasicJavascriptAndAlgorithms/Basic Algorithm Scripting/Boo who.js
@@ -0,0 +1,16 @@
+/*  Question
+
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+
+
+*/
+
+
+function booWho(bool) {
+    let name = "Desmond";
+    return typeof bool=="boolean";
+  }
+  
+  booWho(null);
