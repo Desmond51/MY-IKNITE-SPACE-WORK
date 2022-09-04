0b3f1d33e7c648cca46de5d9435315467b7b0b2e
Desmond <desmondtekum9@gmail.com>
2022-09-04 19:10:15 +0100

    Completed Roman Numeral converter project on FCC

--- MyFreecodecampWork/BasicJavascriptAndAlgorithms/Certification Projects/Roman Numeral Converter.js
+++ MyFreecodecampWork/BasicJavascriptAndAlgorithms/Certification Projects/Roman Numeral Converter.js
@@ -1,61 +1,40 @@
+/*
+Algorithm to convert Roman Numerals to Integer Number:  
 
+Split the Roman Numeral string into Roman Symbols (character).
+Convert each symbol of Roman Numerals into the value it represents.
+Take symbol one by one from starting from index 0: 
+If current value of symbol is greater than or equal to the value of next symbol, then add this value to the running total.
+else subtract this value by adding the value of next symbol to the running total.
+
+*/
 
 
 function convertToRoman(num) {
-    if (num == 'I')
-        return 1;
-    if (num == 'V')
-        return 5;
-    if (num == 'X')
-        return 10;
-    if (num == 'L')
-        return 50;
-    if (num == 'C')
-        return 100;
-    if (num == 'D')
-        return 500;
-    if (num == 'M')
-        return 1000;
-    return -1;
+let romanToNum ={
+M:1000,
+CM:900,
+D:500,
+CD:400,
+C:100,
+XC:90,
+L:50,
+XL:40,
+X:10,
+IX:9,
+V:5,
+IV:4,
+I:1
 }
-
-// Finds decimal value of a
-// given roman numeral
-function romanToDecimal( str)
-{
- 
-    // Initialize result
-    var res = 0;
-
-    for (let i = 0; i < str.length; i++)
-    {
-     
-        // Getting value of symbol s[i]
-        var s1 = convertToRoman(str.charAt(i));
-
-        // Getting value of symbol s[i+1]
-        if (i + 1 < str.length) {
-            var s2 = convertToRoman(str.charAt(i + 1));
-
-            // Comparing both values
-            if (s1 >= s2) {
-               
-                res = res + s1;
-            }
-            else
-            {
-             
-                // Value of current symbol is
-                // less than the next symbol
-                res = res + s2 - s1;
-                i++;
-            }
-        } else {
-            res = res + s1;
-        }
-    }
-
-    return res;
+let roman = "";
+for(let key in romanToNum){
+while(num>=romanToNum[key]){
+roman +=key;
+num -=romanToNum[key];
+}
+}   
+console.log(roman);
+return roman;
 }
 
 convertToRoman(36);
